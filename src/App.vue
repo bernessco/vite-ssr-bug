af783856bd25393c125b629dddcf7f1058186b7e
Mantas Morkūnas <bernessco@recar.lt>
2021-03-22 09:10:14 +0200

    chore: initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link>
+    <router-view v-slot="{ Component }">
+      <Suspense>
+        <component :is="Component" />
+      </Suspense>
+    </router-view>
+  </div>
+</template>
+<script>
+import { useSSRContext } from "vue";
+export default {
+  setup() {
+    useSSRContext();
+  },
+};
+</script>
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
